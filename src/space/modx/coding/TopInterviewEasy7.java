6fb59ff512f33899bc73f6dc7fb5344270f67380
Kevin Xiao <shawzt@gmail.com>
2019-12-24 23:08:49 +0800

    [解答] 给数组元素组成的数字加一

---
+++ src/space/modx/coding/TopInterviewEasy7.java
@@ -0,0 +1,41 @@
+package space.modx.coding;
+
+/*
+加一
+给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+
+最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+
+你可以假设除了整数 0 之外，这个整数不会以零开头。
+
+示例 1:
+
+输入: [1,2,3]
+输出: [1,2,4]
+解释: 输入数组表示数字 123。
+示例 2:
+
+输入: [4,3,2,1]
+输出: [4,3,2,2]
+解释: 输入数组表示数字 4321。
+*/
+
+import java.util.Arrays;
+
+public class TopInterviewEasy7 {
+    public int[] plusOne(int[] digits) {
+        int plus = 1;
+        for (int i = digits.length-1; i >= 0 ; i--) {
+            int sum = digits[i] + plus;
+            plus = sum / 10;
+            digits[i] = sum % 10;
+            if (plus != 1) {
+                return digits;
+            }
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
