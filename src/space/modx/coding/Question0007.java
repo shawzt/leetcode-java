bd2ec6f8fce63f67f234607e881812fb2ae65dc5
Kevin Xiao <shawzt@gmail.com>
2020-01-03 23:41:06 +0800

    [更正] 整数反转，溢出归零

--- src/space/modx/coding/Question0007.java
+++ src/space/modx/coding/Question0007.java
@@ -46,9 +46,9 @@ public class Question0007 {
           int pop = x % 10;
           x /= 10;
 
-          if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE && pop > 7))
+          if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE/10 && pop > 7))
               return 0;
-          if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE && pop < -8))
+          if (result < Integer.MIN_VALUE / 10 || (result == Integer.MIN_VALUE/10 && pop < -8))
               return 0;
 
           result = result * 10 + pop;
