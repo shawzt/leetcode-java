b453596b10693deacb747ef7d2a70694a024d982
Kevin Xiao <shawzt@gmail.com>
2020-01-11 22:51:55 +0800

    [解答] 环形链表

---
+++ src/space/modx/coding/Question0141.java
@@ -0,0 +1,73 @@
+package space.modx.coding;
+
+/*
+环形链表
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+3 -> 2 -> 0 -> -4
+     ^          |
+     |__________|
+
+示例 2：
+
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+1 -> 2
+^    |
+|____|
+
+示例 3：
+
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Question0141 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public boolean hasCycle(ListNode head) {
+        if (null == head || null == head.next) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while (null != fast && null != fast.next) {
+            if (fast == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+}
