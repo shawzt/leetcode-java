fec592da9a2b3c0d095c84603e55d2d8894ad61d
Kevin Xiao <shawzt@gmail.com>
2020-01-16 23:25:17 +0800

    [解答] Fizz Buzz

---
+++ src/space/modx/coding/Question0412.java
@@ -0,0 +1,57 @@
+package space.modx.coding;
+
+/*
+Fizz Buzz
+写一个程序，输出从 1 到 n 数字的字符串表示。
+
+1. 如果 n 是3的倍数，输出“Fizz”；
+
+2. 如果 n 是5的倍数，输出“Buzz”；
+
+3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+
+示例：
+
+n = 15,
+
+返回:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+]
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Question0412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++) {
+            if ( i % 15 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add(i + "");
+            }
+        }
+
+        return result;
+    }
+}
