9679dc50416fb6fee727f9329802eaa7b6f3e33a
Kevin Xiao <shawzt@gmail.com>
2020-02-10 22:15:35 +0800

    [解答] 字母异位分组

---
+++ src/space/modx/coding/Question0049.java
@@ -0,0 +1,43 @@
+package space.modx.coding;
+
+/*
+字母异位分组
+给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+
+示例:
+
+输入: ["eat", "tea", "tan", "ate", "nat", "bat"],
+输出:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+说明：
+
+所有输入均为小写字母。
+不考虑答案输出的顺序。
+*/
+
+import java.util.*;
+
+public class Question0049 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
+
+        Map<String, List<String >> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
