6f4eebed82e763d80f143e864bbcccb5c9368c52
Kevin Xiao <shawzt@gmail.com>
2020-11-17 22:35:11 +0800

    [解答] 根据身高重建队列

---
+++ src/space/modx/coding/Question0406.java
@@ -0,0 +1,45 @@
+package space.modx.coding;
+
+/*
+假设有打乱顺序的一群人站成一个队列。 每个人由一个整数对(h, k)表示，其中h是这个人的身高，k是排在这个人前面且身高大于或等于h的人数。
+编写一个算法来重建这个队列。
+
+注意：
+总人数少于1100人。
+
+示例
+
+输入:
+[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+输出:
+[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+*/
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Question0406 {
+    public int[][] reconstructQueue(int[][] people) {
+        List<int[]> result = new ArrayList<>();
+
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] p1, int[] p2) {
+                if (p1[0] != p2[0]) {
+                   return p2[0] - p1[0];
+                } else {
+                    return p1[1] - p2[1];
+                }
+            }
+        });
+
+        for (int[] p : people) {
+            result.add(p[1], p);
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
